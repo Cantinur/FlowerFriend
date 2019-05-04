235405b3983bd5c30971bc719854e25528a80f1f
Henrik Sandberg <henrik.a.sandberg@icloud.com>
2019-05-04 23:15:59 +0200

    Major changes, now screen and sensors are separate

--- src/FlowerFriend.ino
+++ src/FlowerFriend.ino
@@ -2,30 +2,44 @@
 #include "DS18.h"
 
 
-/************************************ LCD Screen ********************************************/
 LiquidCrystal_I2C *lcd;
+
+DS18 sensor(D3);
+int inputPinLight = A1;
+int inputPinWater = A0;
 int led = D6;
 
-/************************************ Water and water sensor ********************************************/
-int inputPinWater = A0;
 int water = 0;
+double celcius = 0;
+bool notifyUserAboutfire = false;
+bool notifyUserAboutWater = false;
+bool notifyUserAboutTemperature = false;
 
-/************************************ Temprature ********************************************/
-DS18 sensor(D3);
 
-/******************************* For Smoothing Light Sensor Data*************************************************/
+/******************************* Smoothing Light Sensor Data *************************************************/
 const int numberOfReadings = 10;
-int readings[numberOfReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
+int readings[numberOfReadings];
+int readIndex = 0;
+int total = 0;
+int averageLight = 0;
 int dailyHighestAverage = 0;
-int inputPinLight = A1;
+
 
 void setup(void)
 {
   pinMode(inputPinWater, INPUT);
   pinMode(led, OUTPUT);
+
+  //Particle variables
+  Particle.variable("temperature", &celcius, DOUBLE);
+  Particle.variable("water", &water, INT);
+  Particle.variable("light", &averageLight, INT);
+
+  //Particle functions
+  Particle.function("checkNetworkConnection", checkNetworkConnection);
+  Particle.function("checkFireStatus", checkFireStatus);
+  Particle.function("checkWaterStatus", checkWaterStatus);
+  Particle.function("checkTemperatureStatus", checkTemperatureStatus);
 
   //LCD SET UP
   lcd = new LiquidCrystal_I2C(0x27, 16, 2);
@@ -37,75 +51,59 @@ void setup(void)
   for (int thisReading = 0; thisReading < numberOfReadings; thisReading++) {
     readings[thisReading] = 0;
   }
-
-  //Particle.function("WaterLevel")
-
-  // Make sure something is displayed
-  handleWaterLevelOnScreen();
-  
 }
 
-void loop(void)
+void loop()
 {
-  
-  handleWaterLevel();
-  // ligthSensor();
-  // lcd->print(" lumen");
-  handleTemprature();
+  if (hasSensorDataChanged())
+  {
+    updateLED();
+    updateDisplay();  
+  }
   delay(100);
 }
 
-int getCurrentWaterLevel()
+
+/********************************************* SENSOR ***************************************************/
+bool hasSensorDataChanged()
 {
-  return analogRead(inputPinWater)/100;
+  const int currentBestLigth = dailyHighestAverage;
+  const int waterChange = upadetWaterSensorData();
+  const double currentTemp = celcius;
+  updateTermostateData();
+  
+  updateLigthSensorData();
+  if ((currentBestLigth + 10 <= dailyHighestAverage) || (currentBestLigth - 10 >= dailyHighestAverage))
+  {
+    return true;
+  } 
+  else if (currentTemp + 1.00 <= celcius){
+    return true;
+  }
+  else if (currentTemp - 1 >= celcius)
+  {
+    return true;
+  }
+
+  return waterChange;
 }
 
-void handleWaterLevel()
+bool upadetWaterSensorData()
 {
-  const int readWater = getCurrentWaterLevel();
+  const int readWater = analogRead(inputPinWater)/100;
   if (water != readWater)
   {
     water = readWater;
-    handleWaterLevelOnScreen(); 
+    if (water <= 5)
+    {
+      notifyUserAboutWater = true;
+    }
+    return true;
   } 
+  return false;
 }
 
-void handleWaterLevelOnScreen()
-{  
-  lcd->setCursor(1,0);
-  lcd->clear();
-  if (water >= 40)
-  {
-    digitalWrite(led, LOW);
-    lcd->print("Water: Full");
-  }
-  else if (water >= 20)
-  {
-    digitalWrite(led, LOW);
-    lcd->print("Water: Good");
-  }
-  else if (water >= 10)
-  {
-    digitalWrite(led, LOW);
-    lcd->print("Water: OK");
-  }
-  else if (water >= 5)
-  {
-    digitalWrite(led, LOW);
-    lcd->print("Water: Low");
-  }
-  else
-  {
-    digitalWrite(led, HIGH);
-    lcd->print("Water: Bad");
-  }
-
-  //MARK:- TEST display int from water level
-  //lcd->setCursor(0,1);
-  //lcd->print(water);
-}
-
-void ligthSensor()
+void updateLigthSensorData()
 {
   total = total - readings[readIndex];
   readings[readIndex] = analogRead(inputPinLight);
@@ -117,20 +115,143 @@ void ligthSensor()
     readIndex = 0;
   }
 
-  average = total / numberOfReadings;
+  averageLight = total / numberOfReadings;
 
-  lcd->setCursor(0,1);
-  lcd->print(average);
+  if (Time.second() == 0 && Time.hour() == 0)
+  {
+    dailyHighestAverage = 0;
+  }
+  else if (dailyHighestAverage < averageLight)
+  {
+    dailyHighestAverage = averageLight;
+  }
+  
 }
 
-void handleTemprature()
+void updateTermostateData()
 {
   if (sensor.read()) 
   {
-    lcd->setCursor(0,1);
-    lcd->print(sensor.celsius());
-    lcd->print("*C");
-    Particle.variable("temperature", (double) sensor.celsius());
-    Particle.publish("temperature", (double) sensor.celsius(), PUBLIC)
+    celcius = (double) sensor.celsius();
   }
+}
+
+/********************************************* DISPLAY ***************************************************/
+void updateDisplay()
+{
+  lcd->clear();
+
+  const String waterStatus = displayWaterStatus();
+  lcd->setCursor(0,0);
+  lcd->print(waterStatus);
+  
+  int intCelcius = (int) celcius;
+  double differanse = celcius - (double) intCelcius;
+  int add = (differanse >= 0.6) ? 1 : 0;
+  intCelcius += add;
+
+  lcd->print(" ");
+  lcd->print(intCelcius);
+  lcd->print("*C");
+
+  const String lightStatus = displayLightStatus();
+  lcd->setCursor(0,1);
+  lcd->print(lightStatus);
+}
+
+String displayWaterStatus()
+{  
+  String message = "Water: ";
+  if (water >= 40)
+  {
+    message = message + "Full";
+  }
+  else if (water >= 20)
+  {
+    message = message + "Good";
+  }
+  else if (water >= 10)
+  {
+    message = message + "OK";
+  }
+  else if (water >= 5)
+  {
+    message = message + "Low";
+  }
+  else
+  {
+    message = message + "None";
+  }
+  return message;
+}
+
+String displayLightStatus()
+{
+  String message = "Light: ";
+  if (dailyHighestAverage >= 3700)
+  {
+    message = message + "A lot";
+  }
+  else if (dailyHighestAverage >= 2000)
+  {
+    message = message + "Good";
+  }
+  else if (dailyHighestAverage >= 1200)
+  {
+    message = message + "OK";
+  }
+  else if (dailyHighestAverage >= 1200)
+  {
+    message = message + "Low";
+  }
+  else
+  {
+    message = message + "Nothing";
+  }
+  return message;
+}
+
+/********************************************* LED ***************************************************/
+void updateLED()
+{
+  digitalWrite(led, (water <= 5) ? HIGH : LOW);
+}
+
+/********************************************* CHECK STATUS ***************************************************/
+int checkNetworkConnection(String extra)
+{
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  return 0;
+}
+
+int checkFireStatus(String extra)
+{
+  if (notifyUserAboutfire) 
+  {
+    notifyUserAboutfire = false;
+    return 1;
+  }
+  return 0;
+}
+
+int checkWaterStatus(String extra)
+{
+  if (notifyUserAboutWater)
+  {
+    notifyUserAboutWater = false;
+    return 1;
+  }
+  return 0;
+}
+
+int checkTemperatureStatus(String extra)
+{
+  if (notifyUserAboutTemperature)
+  {
+    notifyUserAboutTemperature = false;
+    return 1;
+  }
+  return 0;
 }
